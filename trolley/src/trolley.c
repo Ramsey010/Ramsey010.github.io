d363b1d6133ff81013294c0141639e2edc238683
Ramsey010 <153716360+Ramsey010@users.noreply.github.com>
2024-10-24 14:01:02 +0300

    Update trolley.c

--- trolley/src/trolley.c
+++ trolley/src/trolley.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
 
+
+
+int main(){
+        int l_cost= -1;
+        int r_cost = -1;
+        printf("Please enter the cost of going left\n");
+        if(scanf(" %d", &l_cost)==EOF){
+                  return 0;
+        }
+         if (scanf(" %d", &l_cost) != 1) {
+             printf("No valid left cost provided. Terminating");
+             return 1;
+
+
+
+
+        printf("Please enter the cost of going right\n");
+
+        scanf(" %d", &r_cost);
+        if(l_cost == -1 || r_cost == -1){
+                return 1;
+        }
+
+        if(l_cost<=r_cost){
+                printf("Go left\n");
+        }
+        else{
+                printf("Go right\n");
+        }
+
+
+
+}
