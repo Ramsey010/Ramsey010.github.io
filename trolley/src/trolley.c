139bbd3944c01d7429a8853b2fc866481b052a3b
Ramsey010 <andronikosml@gmail.com>
2024-10-30 17:42:56 +0200

    Your commit message here

--- trolley/src/trolley.c
+++ trolley/src/trolley.c
@@ -1,1 +1,45 @@
+#include <stdio.h>
 
+
+
+int main(){
+	
+	int l_cost;
+	int r_cost;
+	
+	
+	
+	printf("Please enter the cost of going left\n");
+	int result_l = scanf(" %d", &l_cost); 
+	if(result_l==EOF){
+		 printf("End of file. Terminating\n");	
+		       	return 0;
+	}
+	 
+	if(result_l != 1){
+             printf("No valid left cost provided. Terminating\n"); 
+      	     return 1;
+	 }
+	
+	
+	
+	printf("Please enter the cost of going right\n");
+
+        int result_r= scanf(" %d",&r_cost);
+        
+       	if(result_r != 1){
+	        printf("No valid right cost provided. Terminating\n");
+                return 1;
+		  }
+				   
+				   
+         if(l_cost<=r_cost){
+		printf("Go left\n");
+	}
+	 else{
+		printf("Go right\n");
+	}
+       return 0;
+
+
+}
